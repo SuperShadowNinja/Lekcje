91e1039880125656ee482f4e12d03d1a23eda33a
SuperShadowNinja <dawidosek1995-1995@o2.pl>
2014-11-26 20:01:22 +0100

    klasa math

---
+++ dawid/math.php
@@ -0,0 +1,17 @@
+<?php
+class math{
+    public function dodawanie($a,$b){
+        return $a+$b;
+    }
+    public function odejmowanie($a,$b){
+        return $a-$b;
+    }
+    public function mnożenie($a,$b){
+        return $a*$b;
+    }
+    public function dzielenie($a,$b){
+        return $a/$b;
+    }
+}
+
+?>
